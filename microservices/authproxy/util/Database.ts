ae46e73acd3fd4658c8b1c1f628b6a14aa4598d4
Patrick Kloss <patrick.kloss@unitb.de>
2025-04-28 16:43:19 +0200

    fix database access

--- microservices/authproxy/util/Database.ts
+++ microservices/authproxy/util/Database.ts
@@ -4,18 +4,18 @@ import {loadUser, createUser} from "./UsersService";
 const defaultAdminPassword = "admin";
 
 export async function connectToMongoDatabase() {
-    mongoose.connect("mongodb://localhost:27017")
+    mongoose.connect("mongodb://mongodb:27017/strategy-game")
         .then(() => console.log("Connected to MongoDB"))
         .catch(err => console.error("Failed to connect:", err));
 
-    const adminUser = loadUser("admin");
+    const adminUser = await loadUser("admin");
     if (adminUser !== null) {
         console.log("Admin user already exists");
         return;
     }
 
     const tryCreatingAdmin = await createUser("admin", defaultAdminPassword);
-    if(tryCreatingAdmin === null) {
+    if (tryCreatingAdmin === null) {
         console.log("Failed to create admin user");
     }
 }
