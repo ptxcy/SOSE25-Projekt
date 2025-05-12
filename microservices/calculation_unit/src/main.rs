6ad5933412b973168a79cb396786e8b1c687c979
Jonas Kernchen <kernchen.jonas@gmail.com>
2025-05-12 20:57:38 +0200

    code cleanup

--- microservices/calculation_unit/src/main.rs
+++ microservices/calculation_unit/src/main.rs
@@ -27,17 +27,15 @@ async fn main() {
 
 
 	let ws_route_msgpack = warp::path!("msgpack")
-    .and(warp::ws())
-    .map(move |ws: warp::ws::Ws| {
-        // channels for ServerMessages which update this client
-        let sender_sender_clone = server_message_sender_sender.clone();
+	.and(warp::ws())
+	.map(move |ws: warp::ws::Ws| {
+		// channels for ServerMessages which update this client
+		let sender_sender_clone = server_message_sender_sender.clone();
 		let client_message_sender_clone = client_message_sender.clone();
 
 
-        ws.on_upgrade(move |websocket| handle_ws_msgpack(websocket, client_message_sender_clone, sender_sender_clone))
-    });
-
-	// let static_files = warp::fs::dir("public");
+		ws.on_upgrade(move |websocket| handle_ws_msgpack(websocket, client_message_sender_clone, sender_sender_clone))
+	});
 
 	let routes = ws_route_msgpack
 		// .or(static_files)
@@ -46,68 +44,61 @@ async fn main() {
 	warp::serve(routes).run(([0, 0, 0, 0], 8082)).await;
 }
 
+pub fn send_client_message(client_message_sender: &Sender<ClientMessage>, message: &ClientMessage) {
+	let client_message_clone = message.clone();
+	let client_message_sender_clone = client_message_sender.clone();
+	tokio::spawn(async move {
+		client_message_sender_clone.send(client_message_clone).await.log().unwrap();
+	});
+}
+
+pub fn parse_client_message(message: Message) -> Result<ClientMessage, rmp_serde::decode::Error> {
+	let msgpack_bytes = message.into_bytes();
+	rmp_serde::from_slice::<ClientMessage>(&msgpack_bytes[..])
+}
+
 // actual message handling
 async fn handle_ws_msgpack(ws: WebSocket, client_message_sender: Sender<ClientMessage>, sender_sender: Sender<ServerMessageSenderChannel>) {
 	let (mut websocket_tx, mut websocket_rx) = ws.split();
-    let (server_message_tx, mut server_message_rx) = channel::<Arc<ServerMessage>>(32);
+	let (server_message_tx, mut server_message_rx) = channel::<Arc<ServerMessage>>(32);
 
 	// receiving messages from async client
 	tokio::spawn(async move {
 
 		while let Some(Ok(msg)) = websocket_rx.next().await {
-			let msgpack_bytes = msg.into_bytes();
-			let client_message = match rmp_serde::from_slice::<ClientMessage>(&msgpack_bytes[..]).log() {
-			    Ok(m) => m,
-			    Err(_) => continue,
-			};
+			let client_message = match parse_client_message(msg) { Ok(m) => m, Err(_) => continue, };
 
 			match &client_message.request_data {
-		        calculation_unit::messages::client_message::ClientRequest::SpawnDummy { id } => {
+				calculation_unit::messages::client_message::ClientRequest::SpawnDummy { id } => {
 					// send the server_message_tx to the calculation task
 					let id = id.clone();
-			        tokio::spawn(async move {
-			        	println!("new connection");
-						let server_message_tx_clone = server_message_tx.clone();
-			            if let Err(e) = sender_sender.send(ServerMessageSenderChannel::new(id, server_message_tx_clone)).await {
-			                eprintln!("Failed to send server_message_tx: {}", e);
-			            }
-			        });
-			        // make sure only doing this once ever, second loop for other messages
-					// send client message to calculation task
-					let client_message_clone = client_message.clone();
-					let client_message_sender_clone = client_message_sender.clone();
 					tokio::spawn(async move {
-						client_message_sender_clone.send(client_message_clone).await.log().unwrap();
+						println!("new connection");
+						let server_message_tx_clone = server_message_tx.clone();
+						if let Err(e) = sender_sender.send(ServerMessageSenderChannel::new(id, server_message_tx_clone)).await {
+							eprintln!("Failed to send server_message_tx: {}", e);
+						}
 					});
-		            break;
-		        },
-		        _ => { }
-		    };
+					// make sure only doing this once ever, second loop for other messages
+					// send client message to calculation task
+					send_client_message(&client_message_sender, &client_message);
+					break;
+				},
+				_ => { }
+			};
 			
 			// send client message to calculation task
-			let client_message_clone = client_message.clone();
-			let client_message_sender_clone = client_message_sender.clone();
-			tokio::spawn(async move {
-				client_message_sender_clone.send(client_message_clone).await.log().unwrap();
-			});
+			send_client_message(&client_message_sender, &client_message);
 		}
 
 		while let Some(Ok(msg)) = websocket_rx.next().await {
-			let msgpack_bytes = msg.into_bytes();
-			let client_message = match rmp_serde::from_slice::<ClientMessage>(&msgpack_bytes[..]).log() {
-			    Ok(m) => m,
-			    Err(_) => continue,
-			};
+			let client_message = match parse_client_message(msg) { Ok(m) => m, Err(_) => continue, };
 
 			// let received = serde_json::to_string(&client_message).log().unwrap();
 			// println!("Received: {}", received);
 			
 			// send client message to calculation task
-			let client_message_clone = client_message.clone();
-			let client_message_sender_clone = client_message_sender.clone();
-			tokio::spawn(async move {
-				client_message_sender_clone.send(client_message_clone).await.log().unwrap();
-			});
+			send_client_message(&client_message_sender, &client_message);
 		}
 	});
 
