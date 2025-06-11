2918387f28776df40b94a0bb74bd54791548a50f
Patrick Kloss <patrick.kloss@unitb.de>
2025-06-11 15:57:50 +0200

    dynamic porting for playtest calcunit

--- microservices/playtest_calculation_unit/server.ts
+++ microservices/playtest_calculation_unit/server.ts
@@ -1,17 +1,21 @@
 import {WebSocketServer, WebSocket, RawData} from "ws";
 import {ServerMessage} from "./src/datatypes/MessagePackDataTypes";
 import {encode} from "@msgpack/msgpack";
+
 let GAME_CONTEXT_MESSAGE: ServerMessage | null = null;
 import "./src/gamelogic/LogicInterface";
 
 export const FPS: number = 120;
-const wss = new WebSocketServer({port: 9000});
+const port: number = parseInt(process.env.PORT || "9000", 10);
+const wss = new WebSocketServer({ port });
 wss.on("connection", async (ws: WebSocket, req) => {
     ws.on("message", async (message: RawData) => {
+        console.log("Received message", message);
         return;
     })
 
     ws.on("close", () => {
+        console.log("Connection closed");
         return;
     });
     sendPeriodicRenderMessages(ws);
@@ -29,6 +33,6 @@ export function getGameContext(): ServerMessage | null {
     return GAME_CONTEXT_MESSAGE;
 }
 
-export function setGameContext(msg: ServerMessage){
+export function setGameContext(msg: ServerMessage) {
     GAME_CONTEXT_MESSAGE = msg;
 }
