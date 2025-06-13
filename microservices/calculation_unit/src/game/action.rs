db40acba48b18180cfcd533ba1cf5200d9738a0d
jonas kernchen <kernchen.jonas@gmail.com>
2025-06-14 00:07:58 +0200

    cleanup

--- microservices/calculation_unit/src/game/action.rs
+++ microservices/calculation_unit/src/game/action.rs
@@ -54,57 +54,48 @@ impl ActionWrapper {
 
 pub struct AddValue<T: AddAssign + Copy>(*mut T, T);
 impl<T: AddAssign + Copy> AddValue<T> {
-	fn get_execute() -> ActionFn {
-        fn execute<T: AddAssign + Copy>(data: *mut (), _game_objects: *mut GameObjects) {
-            let data = data as *mut AddValue<T>;
-            unsafe {
-                *(*data).0 += (*data).1;
-            }
+    fn execute(data: *mut (), _game_objects: *mut GameObjects) {
+        let data = data as *mut AddValue<T>;
+        unsafe {
+            *(*data).0 += (*data).1;
         }
-        execute::<T>
     }
     pub fn new(target: *mut T, value: T) -> ActionWrapper {
 		ActionWrapper {
 			data: malloc(AddValue(target, value)),
-			func: Self::get_execute()
+			func: Self::execute
 		}
 	}
 }
 
 pub struct SubValue<T: SubAssign + Copy>(*mut T, T);
 impl<T: SubAssign + Copy> SubValue<T> {
-	fn get_execute() -> ActionFn {
-        fn execute<T: SubAssign + Copy>(data: *mut (), _game_objects: *mut GameObjects) {
-            let data = data as *mut SubValue<T>;
-            unsafe {
-                *(*data).0 -= (*data).1;
-            }
+    fn execute(data: *mut (), _game_objects: *mut GameObjects) {
+        let data = data as *mut SubValue<T>;
+        unsafe {
+            *(*data).0 -= (*data).1;
         }
-        execute::<T>
     }
     pub fn new(target: *mut T, value: T) -> ActionWrapper {
 		ActionWrapper {
 			data: malloc(SubValue(target, value)),
-			func: Self::get_execute()
+			func: Self::execute
 		}
 	}
 }
 
 pub struct SetValue<T: Copy>(*mut T, T);
 impl<T: Copy> SetValue<T> {
-	fn get_execute() -> ActionFn {
-        fn execute<T: Copy>(data: *mut (), _game_objects: *mut GameObjects) {
-            let data = data as *mut SetValue<T>;
-            unsafe {
-                *(*data).0 = (*data).1;
-            }
+    fn execute(data: *mut (), _game_objects: *mut GameObjects) {
+        let data = data as *mut SetValue<T>;
+        unsafe {
+            *(*data).0 = (*data).1;
         }
-        execute::<T>
     }
     pub fn new(target: *mut T, value: T) -> ActionWrapper {
 		ActionWrapper {
 			data: malloc(SetValue(target, value)),
-			func: Self::get_execute()
+			func: Self::execute
 		}
 	}
 }
@@ -127,9 +118,11 @@ pub enum SafeAction {
 unsafe impl Send for SafeAction {}
 
 impl SafeAction {
-	fn execute(self, game_objects: *mut GameObjects) {
+	fn execute(data: *mut (), game_objects: *mut GameObjects) {
+		let v = data as *mut SafeAction;
+		let sa = unsafe {(*v).clone()};
 		let go = unsafe { &mut *game_objects };
-		match self {
+		match sa {
 			SafeAction::SpawnFactory { region, factory } => {
 				let region = unsafe { &mut *region };
 				region.factories.push(factory);
@@ -146,18 +139,10 @@ impl SafeAction {
 			}
 		}
 	}
-	fn get_execute() -> ActionFn {
-		fn execute(data: *mut (), game_objects: *mut GameObjects) {
-			let v = data as *mut SafeAction;
-			let sa = unsafe {(*v).clone()};
-			sa.execute(game_objects);
-		}
-		execute
-	}
     pub fn new(sa: SafeAction) -> ActionWrapper {
 		ActionWrapper {
 			data: malloc(sa),
-			func: Self::get_execute()
+			func: Self::execute
 		}
 	}
 }
@@ -169,26 +154,20 @@ pub enum UnsafeAction {
 }
 
 impl UnsafeAction {
-	fn execute(self, game_objects: *mut GameObjects) {
+	fn execute(data: *mut (), game_objects: *mut GameObjects) {
+		let v = data as *mut UnsafeAction;
+		let usa = unsafe {(*v).clone()};
 		let go = unsafe { &mut *game_objects };
-		match self {
+		match usa {
 			UnsafeAction::DeleteSpaceship(index) => {
 				go.spaceships.remove(&index);
 			},
 		}
 	}
-	fn get_execute() -> ActionFn {
-		fn execute(data: *mut (), game_objects: *mut GameObjects) {
-			let v = data as *mut UnsafeAction;
-			let usa = unsafe {(*v).clone()};
-			usa.execute(game_objects);
-		}
-		execute
-	}
     pub fn new(usa: UnsafeAction) -> ActionWrapper {
 		ActionWrapper {
 			data: malloc(usa),
-			func: Self::get_execute()
+			func: Self::execute
 		}
 	}
 }
