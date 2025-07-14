8dc02387f9d77adede945a6810ffbe9ca4c2d08a
Patrick Kloss <patrick.kloss@unitb.de>
2025-07-14 18:31:55 +0200

    mac port funkt

--- client/build-for-macos.sh
+++ client/build-for-macos.sh
@@ -1,14 +1,23 @@
-# cd core
-# mkdir -p include
-# cd ..
+#!/bin/bash
+
+echo "Installing dependencies via Homebrew..."
+brew install sdl2
 brew install glew
 brew install glm
 brew install msgpack-cxx
-brew install meson
-mkdir -p lib
-cd lib
-mkdir -p build_mac
-cd build_mac
+brew install freetype
+brew install cpr
+
+echo "Creating folders..."
+mkdir -p core/include
+mkdir -p lib/build_mac
+
+echo "Downloading stb_image.h..."
+curl -o core/include/stb_image.h https://raw.githubusercontent.com/nothings/stb/master/stb_image.h
+
+echo "Running CMake build..."
+cd lib/build_mac
 cmake -DCMAKE_BUILD_TYPE=Debug ../..
 cmake --build .
 
+echo "Build finished!"
