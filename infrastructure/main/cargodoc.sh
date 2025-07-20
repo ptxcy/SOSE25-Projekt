c9e0cde84081c7b86f486e014adf4cd58782e135
Patrick Kloss <patrick.kloss@unitb.de>
2025-07-20 10:59:27 +0200

    change cargo pipeline name and ip adress for ssh connection

--- infrastructure/main/cargodoc.sh
+++ infrastructure/main/cargodoc.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 PRIVATE_KEY=$1
@@ -10,7 +9,7 @@ ls -la /home/runner
 echo "$PRIVATE_KEY" > ~/.ssh/temp_key.pem
 chmod 400 ~/.ssh/temp_key.pem
 
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/temp_key.pem ec2-user@ec2-3-124-115-76.eu-central-1.compute.amazonaws.com << EOF
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/temp_key.pem ec2-user@ec2-18-196-124-42.eu-central-1.compute.amazonaws.com << EOF
   cd /home/ec2-user/SOSE25-Projekt
   cd ./microservices/calculation_unit && cargo doc && cd ..
   cd ./pong && cargo doc
