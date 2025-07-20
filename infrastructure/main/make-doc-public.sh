b3520911d63cfb2e2c1f199c9d890468111dd97e
Patrick Kloss <patrick.kloss@unitb.de>
2025-07-20 19:22:39 +0200

    make public with nginx

---
+++ infrastructure/main/make-doc-public.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+PRIVATE_KEY=$1
+
+echo "Pipeline is running: Configuring Nginx to serve Doxygen HTML directly"
+echo "$PRIVATE_KEY" > ~/.ssh/temp_key.pem
+chmod 400 ~/.ssh/temp_key.pem
+
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/temp_key.pem ec2-user@ec2-18-196-124-42.eu-central-1.compute.amazonaws.com << EOF
+  echo "Server connected via SSH"
+
+  # Nginx-Konfiguration anlegen
+  sudo tee /etc/nginx/conf.d/doxygen.conf > /dev/null << 'NGINXCONF'
+server {
+    listen 80;
+    server_name localhost;
+
+    location /doxygen/ {
+        root /home/ec2-user/client/doc/;
+        index index.html;
+        try_files \$uri \$uri/ =404;
+    }
+}
+NGINXCONF
+
+  # Zugriffsrechte sicherstellen
+  sudo chmod o+x /home/ec2-user
+  sudo chmod -R o+rX /home/ec2-user/client/doc/doxygen-doc
+
+  # nginx testen und neu laden
+  sudo nginx -t && sudo systemctl reload nginx
+  echo "Nginx wurde konfiguriert und neu geladen"
+EOF
+
+rm -f ~/.ssh/temp_key.pem
